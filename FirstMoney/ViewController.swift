63433402950bb23d5d42d45f8f501f6acadd52b9
Zorigto Badmain <badmainzzz@mail.ru>
2020-10-13 19:27:38 +0900

    Initial Commit

---
+++ FirstMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstMoney
+//
+//  Created by Зоригто Бадмаин on 13.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
