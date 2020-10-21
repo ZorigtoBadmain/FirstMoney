db772280a6c34347c739e4c0a854e5c4219675b5
Zorigto Badmain <badmainzzz@mail.ru>
2020-10-21 10:23:05 +0900

    first commit

---
+++ FirstMoney/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  FirstMoney
+//
+//  Created by Зоригто Бадмаин on 13.10.2020.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var recordImage: UIImageView!
+    @IBOutlet weak var recordCategory: UILabel!
+    @IBOutlet weak var recordCost: UILabel!
+    
+}
